a532f7da06e65f4dc26061c54788c1c6ca3a8161
manman4 <manchanr4@gmail.com>
2020-07-23 23:10:58 +0900

    短く

--- 336282/336282_01.py
+++ 336282/336282_01.py
@@ -4,7 +4,6 @@ class EquivalenceClass:
         self.count = count
 
 def extendedSig(seq, key, n):
-    # keyをlistへ
     key = eval(key)
     top = seq.index(n - 1)
     attachement = top - 1
@@ -14,31 +13,34 @@ def extendedSig(seq, key, n):
             key.insert(top, 2)
             return key
 
-# listをkeyにできない
-e_list = [{"[2]": EquivalenceClass([0], 1)}]
+e_list = [{"[2]": EquivalenceClass([0], 1)}, {}]
 
 def A(n):
     if n < 2:
         return 1
-    e_list.append({})
-    dic = e_list[n - 2]
-    el = e_list[n - 1]
-    for key in dic:
-        print([n, key, dic[key].example, dic[key].count])
-        seq = dic[key].example
+    el_0 = e_list[0]
+    el = e_list[1]
+    for key in el_0:
+        seq = el_0[key].example
         for j in range(n - 1, 0, -1):
             p = seq[0:j] + [n - 1] + seq[j:]
             res = extendedSig(p, key, n)
             if not res:
                 break
             s = str(res)
-            c = dic[key].count
+            c = el_0[key].count
             if s in el:
                 el[s].count += c
             else:
                 el[s] = EquivalenceClass(p, c)
 
+    e_list[0] = el
+    e_list[1] = {}
     return sum(el[key].count for key in el)
 
-for k in range(6):
-    print(A(k))
+def A336282List(size):
+    return [A(k) for k in range(size)]
+
+print(A336282List(12))
+
+
