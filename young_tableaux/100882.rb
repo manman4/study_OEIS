e66fb62588155a88292262753787f1a865582bdd
manman4 <manchanr4@gmail.com>
2020-08-22 15:09:56 +0900

    A100882,A100883を比較しながらコードをきれいにする。

--- young_tableaux/100882.rb
+++ young_tableaux/100882.rb
@@ -1,12 +1,10 @@
 def f(n, i, t)
   return 0 if n < 0
   return 1 if n == 0
+  # A209915とは特にここが異なる
   if i == 1
-    if n <= t
-      return 1
-    else
-      return 0
-    end
+    return 1 if n <= t
+    return 0
   end
   return 0 if i == 0
   return f(n, i - 1, t) + (1..[t, n / i].min).inject(0){|s, j| s + f(n - i * j, i - 1, j)}
@@ -16,4 +14,4 @@ def A(n)
   f(n, n, n)
 end
 
-p (0..20).map{|i| A(i)}
+p (0..50).map{|i| A(i)}
--- young_tableaux/100883.rb
+++ young_tableaux/100883.rb
@@ -2,11 +2,8 @@ def f(n, i, t)
   return 0 if n < 0
   return 1 if n == 0
   if i == 1
-    if n >= t
-      return 1
-    else
-      return 0
-    end
+    return 1 if n >= t # A100882とは不等号の向きが異なる
+    return 0
   end
   return 0 if i == 0
   return f(n, i - 1, t) + (t..n / i).inject(0){|s, j| s + f(n - i * j, i - 1, j)}
@@ -16,4 +13,4 @@ def A(n)
   f(n, n, 1)
 end
 
-p (0..20).map{|i| A(i)}
+p (0..50).map{|i| A(i)}
