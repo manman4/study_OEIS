fd7b8a41a2a9ed43ef545e2854ae97b794b06b1c
manman4 <manchanr4@gmail.com>
2020-09-13 18:33:20 +0900

    A335547の変形（自身を除き３パターン）のコードは以下のとおり 335547_02.py, 335547_03.py

---
+++ staircase_polyomino/335547_03.py
@@ -0,0 +1,21 @@
+def f(n):
+    coords = {(x, y) for x in range(n) for y in range(n) if x <= y}
+    pieces = []
+    for size in range(1, n+1):
+        b0 = {(x, y) for x in range(size) for y in range(size) if x <= y}
+        b1 = {(size-1-x, size-1-y) for x, y in b0}
+        pieces += [{(x+ox, y+oy) for x, y in b1} for ox in range(n) for oy in range(n)]
+    pieces = [p for p in pieces if p <= coords]
+    pieces.sort(key=min)
+    
+    def recurse(grid, candidates, depth=1):
+        if not grid: return 1
+        if not candidates: return 0
+        hd, tl = candidates[0], candidates[1:]
+        if min(grid) != min(hd): return 0
+        if hd <= grid: return recurse(grid - hd, [t for t in tl if not (hd & t)], depth+1) + recurse(grid, tl, depth+1)
+        return recurse(grid, tl, depth+1)
+    return recurse(coords, pieces)
+
+for i in range(1, 9):
+    print([i, f(i)])
