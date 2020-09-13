0b924657c47fecc3c28374c0cc54a400d0e99863
manman4 <manchanr4@gmail.com>
2020-09-13 14:51:11 +0900

    微修正

--- src/334/334617/334617_01.py
+++ src/334/334617/334617_01.py
@@ -1,11 +1,11 @@
 def f(n):
     coords = {(x, y) for x in range(n) for y in range(n) if x <= y}
     pieces = []
-    for size in range(1,n+1):
-        b0 = {(x,y) for x in range(size) for y in range(size) if x <= y}
-        b1 = {(       x,size-1-y) for x,y in b0}
-        b2 = {(size-1-x,       y) for x,y in b0}
-        b3 = {(size-1-x,size-1-y) for x,y in b0}
+    for size in range(1, n+1):
+        b0 = {(x, y) for x in range(size) for y in range(size) if x <= y}
+        b1 = {(       x, size-1-y) for x, y in b0}
+        b2 = {(size-1-x,        y) for x, y in b0}
+        b3 = {(size-1-x, size-1-y) for x, y in b0}
         bases = [b0] if size == 1 else [b0, b1, b2, b3]
         pieces += [{(x+ox, y+oy) for x, y in b} for b in bases for ox in range(n) for oy in range(n)]
     pieces = [p for p in pieces if p <= coords]
