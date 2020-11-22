5b0a398e841fbd937017e0378b8b96b947031e32
manman4 <manchanr4@gmail.com>
2020-11-23 00:46:50 +0900

    Number of Hamiltonian cycles in the graph C_n X P_n.

---
+++ graphillion/222197.py
@@ -0,0 +1,21 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_CnXPk(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+        grids.append((i + (n - 1) * k, i))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A222197(n):
+    universe = make_CnXPk(n, n)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles(is_hamilton=True)
+    return cycles.len()
+
+print([A222197(n) for n in range(3, 7)])
