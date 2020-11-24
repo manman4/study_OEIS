ed0bc9b9d109ea1acdb8b64325f113024497d4b2
manman4 <manchanr4@gmail.com>
2020-11-24 19:52:46 +0900

    Number of Hamiltonian cycles in P_8 X P_n.

---
+++ graphillion/145418.py
@@ -0,0 +1,20 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_PnXPk(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A(n):
+    universe = make_PnXPk(8, n)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles(is_hamilton=True)
+    return cycles.len()
+
+print([A(n) for n in range(2, 7)])
