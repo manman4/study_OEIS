23a468350b794d44aba1ba6e9df948f1dd2915d6
manman4 <manchanr4@gmail.com>
2020-11-28 22:00:20 +0900

    Number of (undirected) cycles on the n X 5 king graph.

---
+++ graphillion/339199.py
@@ -0,0 +1,27 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_nXk_king_graph(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+            if i < k:
+                grids.append((i + (j - 1) * k, i + j * k + 1))
+            if i > 1:
+                grids.append((i + (j - 1) * k, i + j * k - 1))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A339098(n, k):
+    universe = make_nXk_king_graph(n, k)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles()
+    return cycles.len()
+
+def A339199(n):
+    return A339098(n, 5)
+
+print([A339199(n) for n in range(2, 20)])
