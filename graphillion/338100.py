eff53c031ca2252525cf508cd9d12b9330562dee
manman4 <manchanr4@gmail.com>
2020-11-29 22:42:48 +0900

    Number of spanning trees in the n X 2 king graph.

---
+++ graphillion/338100.py
@@ -0,0 +1,28 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_nXk_king_graph(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+            if i < k:
+                grids.append((i + (j - 1) * k, i + j * k + 1))
+            if i > 1:
+                grids.append((i + (j - 1) * k, i + j * k - 1))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A338029(n, k):
+    if n == 1 or k == 1: return 1
+    universe = make_nXk_king_graph(n, k)
+    GraphSet.set_universe(universe)
+    spanning_trees = GraphSet.trees(is_spanning=True)
+    return spanning_trees.len()
+
+def A338100(n):
+    return A338029(n, 2)
+
+print([A338100(n) for n in range(1, 20)])
