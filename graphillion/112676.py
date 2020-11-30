f03dc94fc3145aedebf73b6d89e2c73641fa2f11
manman4 <manchanr4@gmail.com>
2020-12-01 00:50:58 +0900

    Number of (undirected) Hamiltonian cycles on a triangular grid, n vertices on each side.

---
+++ graphillion/112676.py
@@ -0,0 +1,21 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_n_triangular_grid_graph(n):
+    s = 1
+    grids = []
+    for i in range(n + 1, 1, -1):
+        for j in range(i - 1):
+            a, b, c = s + j, s + j + 1, s + i + j
+            grids.extend([(a, b), (a, c), (b, c)])
+        s += i
+    return grids
+
+def A112676(n):
+    if n == 1: return 1
+    universe = make_n_triangular_grid_graph(n - 1)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles(is_hamilton=True)
+    return cycles.len()
+
+print([A112676(n) for n in range(1, 12)])
