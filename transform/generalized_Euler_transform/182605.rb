3e83c50f54b489da8618b791a44514a64f742399
manman4 <manchanr4@gmail.com>
2020-12-06 12:41:52 +0900

    GenET application example

--- transform/generalized_Euler_transform/182605.rb
+++ transform/generalized_Euler_transform/182605.rb
@@ -1,14 +1,16 @@
+# Cf. https://manchanr6.blogspot.com/2017/05/170516.html
+
 def s(f_ary, g_ary, n)
   s = 0
   (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
   s
 end
 
-def A(f_ary, g_ary, n)
-  ary = [1]
-  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
-  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
-  ary
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
 end
 
 n = 50
@@ -17,20 +19,13 @@ ary2 = Array.new(n + 1,  1)
 ary3 = Array.new(n + 1, 11)
 
 # Product_{k>0} (1 - x^k)
-p A(ary1, ary2, n)
+p A([[ary1, ary2]], n)
 
 # Product_{k>0} 1/(1 - 11*x^k)
-p A(ary2, ary3, n)
-
-def A_rational(f1_ary, g1_ary, f2_ary, g2_ary, n)
-  ary = [1]
-  a = [0] + (1..n).map{|i| s(f1_ary, g1_ary, i) + s(f2_ary, g2_ary, i)}
-  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
-  ary
-end
+p A([[ary2, ary3]], n)
 
 # Product_{k>0} (1 - x^k)/(1 - 11*x^k)
-p A_rational(ary1, ary2, ary2, ary3, n)
+p A([[ary1, ary2], [ary2, ary3]], n)
 
 =begin
 Output
