e81330ec5caf518d610235fee85ada50d0c2c555
manman4 <manchanr4@gmail.com>
2020-12-06 13:48:12 +0900

    Expansion of Product_{k>=1} (1 - k*x^k)^k.

---
+++ transform/generalized_Euler_transform/266964.rb
@@ -0,0 +1,21 @@
+# Cf. https://manchanr6.blogspot.com/2017/05/170516.html
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 50
+ary1 = (0..n).map{|i| -i}
+ary2 = (0..n).map{|i|  i}
+
+# Product_{k>0} (1 - k * x^k)^k
+p A([[ary1, ary2]], n)
