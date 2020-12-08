3bd8432f2f92de95362f6b28f247463230572056
manman4 <manchanr4@gmail.com>
2020-12-09 00:04:12 +0900

    Product_{k>0} 1/(1 - x^k)

---
+++ transform/generalized_Euler_transform/000041.rb
@@ -0,0 +1,18 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+n = 50
+ary1 = Array.new(n + 1, 1)
+
+# Product_{k>0} 1/(1 - x^k)
+p A([[ary1, ary1]], n)
