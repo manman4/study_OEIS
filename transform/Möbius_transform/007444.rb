b40afeb8ad3490255121b2f05f710af45aa18949
manman4 <manchanr4@gmail.com>
2020-12-12 13:07:33 +0900

    Moebius transform of primes.

---
+++ transform/Möbius_transform/007444.rb
@@ -0,0 +1,26 @@
+require 'prime'
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+# a[0] = 0
+def Möbius_transform(ary, n)
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  a = [0]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * ary[j] if i % j == 0
+    }
+    a << s
+  }
+  a
+end
+
+n = 50
+ary = [0] + Prime.take(n).to_a
+
+p Möbius_transform(ary, n)
