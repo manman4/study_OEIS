cf1be835bc53af8a39dabdd685b8cace93e9efe4
manman4 <manchanr4@gmail.com>
2020-12-19 20:00:39 +0900

    Number of Hamiltonian paths in P_3 X P_n.

---
+++ graphillion/003685.py
@@ -0,0 +1,31 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_PnXPk(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A(start, goal, n, k):
+    universe = make_PnXPk(n, k)
+    GraphSet.set_universe(universe)
+    paths = GraphSet.paths(start, goal, is_hamilton=True)
+    return paths.len()
+
+def A332307(n, k):
+    m = k * n
+    s = 0
+    for i in range(1, m):
+        for j in range(i + 1, m + 1):
+            s += A(i, j, n, k)
+    return s
+
+def A003685(n):
+    return A332307(3, n)
+
+print([A003685(n) for n in range(1, 11)])
