63b21da8512473a2b95354b2886315c4ef08438c
manman4 <manchanr4@gmail.com>
2020-12-19 20:22:35 +0900

    Array read by antidiagonals: T(m,n) = number of (undirected) paths in the grid graph P_m X P_n.

---
+++ graphillion/288518.py
@@ -0,0 +1,35 @@
+# Using graphillion
+from graphillion import GraphSet
+import graphillion.tutorial as tl
+
+# def make_PnXPk(n, k):
+#     grids = []
+#     for i in range(1, k + 1):
+#         for j in range(1, n):
+#             grids.append((i + (j - 1) * k, i + j * k))
+#     for i in range(1, k * n, k):
+#         for j in range(1, k):
+#             grids.append((i + j - 1, i + j))
+#     return grids
+
+# def A(start, goal, n, k):
+#     universe = make_PnXPk(n, k)
+#     GraphSet.set_universe(universe)
+#     paths = GraphSet.paths(start, goal)
+#     return paths.len()
+
+def A(start, goal, n, k):
+    universe = tl.grid(n - 1, k - 1)
+    GraphSet.set_universe(universe)
+    paths = GraphSet.paths(start, goal)
+    return paths.len()
+
+def A288518(n, k):
+    m = k * n
+    s = 0
+    for i in range(1, m):
+        for j in range(i + 1, m + 1):
+            s += A(i, j, n, k)
+    return s
+
+print([A288518(j + 1, i - j + 1) for i in range(9) for j in range(i + 1)])
