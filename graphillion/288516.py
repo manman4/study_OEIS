65307b4d113c378c101d159f56bbb69f17c56b1f
manman4 <manchanr4@gmail.com>
2020-12-19 20:14:52 +0900

    Number of (undirected) paths in the ladder graph P_2 X P_n.

--- graphillion/288516.py
+++ graphillion/288516.py
@@ -1,18 +1,25 @@
 # Using graphillion
 from graphillion import GraphSet
+import graphillion.tutorial as tl
 
-def make_PnXPk(n, k):
-    grids = []
-    for i in range(1, k + 1):
-        for j in range(1, n):
-            grids.append((i + (j - 1) * k, i + j * k))
-    for i in range(1, k * n, k):
-        for j in range(1, k):
-            grids.append((i + j - 1, i + j))
-    return grids
+# def make_PnXPk(n, k):
+#     grids = []
+#     for i in range(1, k + 1):
+#         for j in range(1, n):
+#             grids.append((i + (j - 1) * k, i + j * k))
+#     for i in range(1, k * n, k):
+#         for j in range(1, k):
+#             grids.append((i + j - 1, i + j))
+#     return grids
+
+# def A(start, goal, n, k):
+#     universe = make_PnXPk(n, k)
+#     GraphSet.set_universe(universe)
+#     paths = GraphSet.paths(start, goal)
+#     return paths.len()
 
 def A(start, goal, n, k):
-    universe = make_PnXPk(n, k)
+    universe = tl.grid(n - 1, k - 1)
     GraphSet.set_universe(universe)
     paths = GraphSet.paths(start, goal)
     return paths.len()
