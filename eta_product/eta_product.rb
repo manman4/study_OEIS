61b3bc9aac913818d8c437622cf8a50b97427b13
manman4 <manchanr4@gmail.com>
2020-12-26 20:15:47 +0900

    eta product

---
+++ eta_product/eta_product.rb
@@ -0,0 +1,14 @@
+# https://manchanr6.blogspot.com/2017/05/170516.html
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
