1c68c0392165bfe14e1c5973e53c2b8076951f6b
manman4 <manchanr4@gmail.com>
2020-12-26 19:30:36 +0900

    Product_{k>0} 1/(1 + x^k)^(k * (3 * k - 2))

--- transform/generalized_Euler_transform/295123.rb
+++ transform/generalized_Euler_transform/295123.rb
@@ -1,15 +1,4 @@
-def s(f_ary, g_ary, n)
-  s = 0
-  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
-  s
-end
-
-def A(ary, n)
-  a_ary = [1]
-  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i)}}
-  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
-  a_ary
-end
+require './generalized_Euler_transform'
 
 n = 50
 ary1 = (0..n).map{|i|  i * (3 * i - 2)}
