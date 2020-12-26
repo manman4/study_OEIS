e5955358283e9391453265f4a94a2008eda87e28
manman4 <manchanr4@gmail.com>
2020-12-26 19:54:09 +0900

    a(n) = Sum_{d|n} mu(n/d)*5^(d-1).

--- transform/Möbius_transform/295506.rb
+++ transform/Möbius_transform/295506.rb
@@ -1,24 +1,4 @@
-require 'prime'
-
-def A008683(n)
-  ary = n.prime_division
-  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
-  0
-end
-
-# a[0] = 0
-def Möbius_transform(ary, n)
-  m_ary = [0] + (1..n).map{|i| A008683(i)}
-  a = [0]
-  (1..n).each{|i|
-    s = 0
-    (1..i).each{|j|
-      s += m_ary[i / j] * ary[j] if i % j == 0
-    }
-    a << s
-  }
-  a
-end
+require './Möbius_transform'
 
 n = 50
 ary = [0] + (1..n).map{|i| 5 ** (i - 1)}
