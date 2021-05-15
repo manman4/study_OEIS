54802c064d397704f2a76425265a1fe8ae7efbcc
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-15 23:43:59 +0900

    d(n) (also called tau(n) or sigma_0(n)), the number of divisors of n.

--- math_func/sigma_0.rb
+++ math_func/sigma_0.rb
@@ -1,9 +1,13 @@
 require 'prime'
 
-def d(n)
-  return 1 if n == 1
+# Cf. A344080
+def tau(n)
   n.prime_division.inject(1){|s, i| s * (i[1] + 1)}
 end
 
-n = 20
-p (1..n).map{|i| d(i)}
+# a[0]=0
+def tau_ary(n)
+  [0] + (1..n).map{|i| tau(i)}
+end
+
+p tau_ary(20)
