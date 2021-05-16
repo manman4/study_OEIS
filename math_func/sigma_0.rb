28ff646028a3420c0899b89ee1b4c68858841ae5
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-16 21:42:15 +0900

    tau(n^k)

--- math_func/sigma_0.rb
+++ math_func/sigma_0.rb
@@ -1,13 +1,18 @@
 require 'prime'
 
 # Cf. A344080
-def tau(n)
-  n.prime_division.inject(1){|s, i| s * (i[1] + 1)}
+# tau(n^k)
+def tau(n, k = 1)
+  n.prime_division.inject(1){|s, i| s * (k * i[1] + 1)}
 end
 
 # a[0]=0
-def tau_ary(n)
-  [0] + (1..n).map{|i| tau(i)}
+def tau_ary(n, k = 1)
+  [0] + (1..n).map{|i| tau(i, k)}
 end
 
 p tau_ary(20)
+# A048691
+p tau_ary(20, 2)
+# A048785
+p tau_ary(20, 3)
