33ac3799d80e345801b89b003186673d2c38a710
manman4 <7023566+manman4@users.noreply.github.com>
2023-10-01 12:02:19 +0900

    fps

--- basic/fps/fps.rb
+++ basic/fps/fps.rb
@@ -18,12 +18,21 @@ def Rational_function(ary0, ary, n)
   a
 end
 
-a = [1, -2] + [0] * 9
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
 
+a = [1, -2] + [0] * 9
 p I(a, 10)
 
 b = [0, 0, 1] + [0] * 8
 p Rational_function(b, a, 10)
 
-
+# A002420
+c = [1, 4] + [0] * 9
+p sqrt_a(c)
     
