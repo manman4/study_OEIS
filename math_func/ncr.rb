fd3abd556dac6b454967644fe9238709b34f2ddf
manman4 <7023566+manman4@users.noreply.github.com>
2024-01-29 20:15:43 +0900

    # 入山徳夫氏によるアルゴリズム

---
+++ math_func/ncr.rb
@@ -0,0 +1,25 @@
+# 入山徳夫氏によるアルゴリズム
+def C(n, r)
+  r = [r, n - r].min
+  return 1 if r == 0
+  return n if r == 1
+  numerator = (n - r + 1..n).to_a
+  denominator = (1..r).to_a
+  (2..r).each{|p|
+    pivot = denominator[p - 1]
+    if pivot > 1
+      offset = (n - r) % p
+      (p - 1).step(r - 1, p){|k|
+        numerator[k - offset] /= pivot
+        denominator[k] /= pivot
+      }
+    end
+  }
+  result = 1
+  (0..r - 1).each{|k|
+    result *= numerator[k] if numerator[k] > 1
+  }
+  return result
+end
+
+p (0..10).map{|i| C(10, i)}
