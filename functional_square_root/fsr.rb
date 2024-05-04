e0e066d80832e9b9dbc4a2b0f085a0bc1ecad7a2
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-05 00:19:22 +0900

    実装

--- functional_square_root/fsr.rb
+++ functional_square_root/fsr.rb
@@ -49,8 +49,8 @@ def f3r(f_ary, n)
   }
   (2..n).each{|i|
     # 計算の順に注意
-    (i - 1).downto(1){|k|
-      b[k][i] = (a[k][i] - (k + 1..i - 1).inject(0){|s, j| s + (j..i).inject(b[j][i]){|t, m| t + b[m][i] * b[j][m]} * b[k][j]}) / 3r
+    (i - 1).downto(1){|x|
+      b[x][i] = (a[x][i] - (x + 1..i - 1).inject(0){|s, j| s + (j..i).inject(b[j][i]){|t, m| t + b[m][i] * b[j][m]} * b[x][j]}) / 3r
     }
   }
   b[1]
