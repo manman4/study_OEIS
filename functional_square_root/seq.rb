91de8c042029a61a83709d167634ad33d402134e
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-14 00:08:26 +0900

    Functional Square Root等

--- functional_square_root/seq.rb
+++ functional_square_root/seq.rb
@@ -2,15 +2,22 @@ require './fsr'
 
 n = 20
 
+# A220110, 
 (2..6).each{|i|
-    f_ary = [0, 1, i * i, i * i * i] + [0] * (n - 3)
-    p a = fsr(f_ary, n, i)
-    p a.map(&:to_i)
+  f_ary = [0, 1, i * i, i * i * i] + [0] * (n - 3)
+  p a = fsr(f_ary, n, i)
+  p a.map(&:to_i)
+}
+
+# A141120, A141121
+(2..6).each{|i|
+  f_ary = [0, 1, i * i] + [0] * (n - 2)
+  p a = fsr(f_ary, n, i)
+  p a.map(&:to_i)
 }
 
 (2..6).each{|i|
-    f_ary = [0, 1, i * i] + [0] * (n - 2)
-    p a = fsr(f_ary, n, i)
-    p a.map(&:to_i)
+  f_ary = [0] + (1..n).map{|j| j * (i * i) ** (j - 1)}
+  p a = fsr(f_ary, n, i)
+  p a.map(&:to_i)
 }
-
