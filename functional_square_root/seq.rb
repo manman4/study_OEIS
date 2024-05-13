1af7a18b48ce1b4afb21e9dd0febb7d99e84286e
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-14 00:19:43 +0900

    Functional Square Root等

--- functional_square_root/seq.rb
+++ functional_square_root/seq.rb
@@ -25,3 +25,17 @@ n = 20
   p a = fsr(f_ary, n, i)
   p [i, a.all?{|i| i.denominator == 1}]
 }
+
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+(2..6).each{|i|
+  f_ary = [0] + (1..n).map{|j| i ** (j - 1) / f(j - 1).to_r}
+  a = fsr(f_ary, n, i)
+  p b = [0] + (1..n).map{|j| f(j) * a[j]}
+  p [i, b.all?{|i| i.denominator == 1}]
+}
+
