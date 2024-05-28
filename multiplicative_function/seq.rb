8436fc1a6b18deb1718e92d095b19265ca4d51c4
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-29 01:26:44 +0900

    Expansion of Sum_{k>=0} x^(7^k) / (1 - x^(7^k)).

--- multiplicative_function/seq.rb
+++ multiplicative_function/seq.rb
@@ -5,7 +5,18 @@ def A372671(n)
   n.prime_division.inject(1){|s, (p, e)| s * (p == 2 || p == 3 ? p ** e : (p - 1) * p ** (e - 1))}
 end
 
+def A(n, prime)
+  return 1 if n == 1
+  n.prime_division.inject(1){|s, (p, e)| s * (p == prime ? e + 1 : 1)}
+end
+
+def A373217(n)
+  A(n, 7)
+end
+
+
 n = 50
 
 p (1..n).map{|i| A372671(i)}
+p (1..n).map{|i| A373217(i)}
 
